18dda895a5374b8bce8488bf57cdc51f9cfa50dc
Jonathan Simms <slyphon@gmail.com>
2019-05-17 12:29:28 -0400

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def main():
 
   setuptools.setup(
       name="slyphon-jupyter-vim-binding",
-      version="0.1.0.dev1",
+      version="0.1.0.dev2",
       author="Jonathan Simms",
       author_email="slyphon@gmail.com",
       description="vim bindings for jupyter (slyphon fork)",
